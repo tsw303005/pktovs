6a33a6243977fcf8e459c611afedf34dce8e40e1
tsw303005 <contact@ivanouo.one>
2023-12-03 17:43:41 +0000

    first commit

---
+++ client.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+import json
+from dotenv import load_dotenv
+
+# load env
+load_dotenv()
+
+n =  int(os.getenv('N')) # n is number of thread
+server_addr = os.getenv('SERVER_ADDR')
+commands = [f"iperf3 -c {server_addr} -p {5100 + i} -J > {5100 + i}.json" for i in range(n)]
+
+processes = [subprocess.Popen(cmd, shell=True) for cmd in commands]
+
+for process in processes:
+    process.wait()
+
+total_transfer_data = 0
+bandwidth = 0
+for i in range(n):
+    with open(f"{5100 + i}.json", "r") as f:
+        data = json.load(f)
+        total_transfer_data += int(data["end"]["sum_sent"]["bytes"]) * 8
+        bandwidth += int(data["end"]["sum_sent"]["bits_per_second"])
+
+print(f"Total transfer data: {total_transfer_data / 1000000000} Gbits")
+print(f"Bandwidth: {bandwidth / 1000000000} Gbits/sec")
---
+++ server.py
@@ -0,0 +1,53 @@
+import subprocess
+import re
+from time import sleep
+
+def getNumSoftirqs(irqNum): 
+    command = "cat /proc/softirqs | grep -i  NET_RX:"
+
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output, err = proc.communicate()
+
+    result = re.sub(r'\s+', ' ', output.decode('utf-8'))
+    result = re.findall(r'\d+', result)
+
+    if len(irqNum) == 0:
+        irqNum = result
+    else:
+        diff = []
+        for index, num in enumerate(irqNum):
+            diff.append(int(result[index]) - int(num))
+        
+        global count
+        count += sum(diff)
+        irqNum = result
+
+    return irqNum
+
+def cpuUsage():
+    command = "sar -u 1"
+    
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output, err = proc.communicate()
+
+    print(output.decode('utf-8'))
+
+
+def startIperfServer(n):
+    commands = [f"iperf3 -s -p {5100 + i} > /dev/null 2>&1 &" for i in range(n)]
+    processes = [subprocess.Popen(cmd, shell=True) for cmd in commands]
+
+    for process in processes:
+        process.wait()
+
+
+n = 7 # n is numer of iperf server
+irqNum = list()
+count = 0
+startIperfServer(n)
+while (True):
+    irqNum = getNumSoftirqs(irqNum)
+    print(count)
+    sleep(1)
